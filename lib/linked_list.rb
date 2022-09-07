76e395320b365e5daf075c7b109e680aeebd0a46
Lucas Duncan <lucasdonuts@gmail.com>
2022-09-07 14:50:36 -0500

    commit tree 571ccad222ff6def9259ca999548c74a517b049b

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,29 @@ class LinkedList
   end
 
   def nth_from_end(n)
-    # your code here
+    return if n > length || n < 1
+
+    current = head
+
+    (length - n).times do
+      current = current.next_node
+    end
+
+    current.value
+  end
+
+  def length
+    return 0 unless head
+
+    current = head
+    i = 1
+
+    until current.next_node.nil?
+      current = current.next_node
+      i += 1
+    end
+
+    i
   end
 
 end
